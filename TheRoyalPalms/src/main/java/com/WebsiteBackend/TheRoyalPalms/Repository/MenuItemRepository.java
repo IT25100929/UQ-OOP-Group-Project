738b805a056bf30dc6e0abd8ea8778eda578e1c0
IT25100929 <it25100929@my.sliit.lk>
2026-05-15 10:52:47 +0530

    More changes

--- TheRoyalPalms/src/main/java/com/WebsiteBackend/TheRoyalPalms/Repository/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WebsiteBackend.TheRoyalPalms.Repository;
-
-import com.WebsiteBackend.TheRoyalPalms.Model.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
